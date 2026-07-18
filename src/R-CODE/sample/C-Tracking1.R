ccfceb8a747886698060a51d9e032ecc14c8c6a8
cartheur <cartheur@pm.me>
2026-07-18 14:02:30 +0200

    Migrate rcode to its own repo

--- src/R-CODE/sample/C-Tracking1.R
+++
@@ -1,9 +0,0 @@
-//-------------------------------------------
-//  Tracking 1
-//-------------------------------------------
-
-SET:Power:1
-POSE:AIBO:slp_slp
-
-SET:COLOR:PINK
-MOVE:HEAD:C-TRACKING:1000
