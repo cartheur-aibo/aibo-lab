502bc0d18b567d072ece9be934c6a7f4f05aa9be
cartheur <cartheur@pm.me>
2026-07-20 18:19:26 +0200

    Organize

--- dev/aibo-em.fs
+++ dev/aibo-em.fs
@@ -1,6 +1,6 @@
 \ Expectation-Maximization clustering demo for gforth.
 \ This is a small 1D Gaussian mixture example intended as a
-\ runnable bridge from the AIBO EM appendix into real Forth code.
+\ runnable bridge from the AIBO histogram and EM notes into real Forth code.
 \ Run on this machine with:
 \   XDG_CACHE_HOME=/tmp gforth-fast --no-0rc scripts/aibo-em.fs
 
@@ -9,6 +9,7 @@ decimal
 12 constant n-samples
 2 constant n-clusters
 50 constant max-iterations
+256 constant histogram-bins
 
 2e0 pi f* fsqrt fconstant SQRT-2PI
 1e-6 fconstant SIGMA-FLOOR
@@ -22,8 +23,16 @@ create means n-clusters floats allot
 create sigmas n-clusters floats allot
 create probs n-samples n-clusters * floats allot
 
+create hist-query histogram-bins floats allot
+create hist-view-a histogram-bins floats allot
+create hist-view-b histogram-bins floats allot
+
 fvariable last-ll
 fvariable current-ll
+fvariable best-score
+fvariable hist-a-bin
+fvariable hist-b-bin
+fvariable hist-denom
 
 : sample-addr ( i -- addr )
   floats samples + ;
@@ -59,6 +68,65 @@ fvariable current-ll
 : prob! ( r i k -- )
   prob-addr f! ;
 
+: hist-bin-addr ( hist i -- addr )
+  locals| i hist |
+  i floats hist + ;
+
+: hist@ ( hist i -- r )
+  hist-bin-addr f@ ;
+
+: hist! ( r hist i -- )
+  hist-bin-addr f! ;
+
+: clear-hist ( hist -- )
+  locals| hist |
+  histogram-bins 0 do
+    0e0 hist i hist!
+  loop ;
+
+: hist-divergence ( hist-a hist-b -- r )
+  locals| hist-b hist-a |
+  0e0
+  histogram-bins 0 do
+    hist-a i hist@ hist-a-bin f!
+    hist-b i hist@ hist-b-bin f!
+    hist-a-bin f@ hist-b-bin f@ f+ hist-denom f!
+    hist-denom f@ SIGMA-FLOOR f<
+    if
+      \ zero denominator contributes nothing
+    else
+      hist-a-bin f@ hist-b-bin f@ f-
+      fdup f*
+      hist-denom f@ f/
+      f+
+    then
+  loop ;
+
+: init-histograms ( -- )
+  hist-query clear-hist
+  hist-view-a clear-hist
+  hist-view-b clear-hist
+  8e0 hist-query 10 hist!
+  7e0 hist-query 11 hist!
+  3e0 hist-query 50 hist!
+  7e0 hist-view-a 10 hist!
+  8e0 hist-view-a 11 hist!
+  3e0 hist-view-a 50 hist!
+  1e0 hist-view-b 180 hist!
+  8e0 hist-view-b 181 hist!
+  6e0 hist-view-b 220 hist! ;
+
+: nearest-view ( -- n )
+  hist-query hist-view-a hist-divergence
+  fdup best-score f!
+  hist-query hist-view-b hist-divergence
+  best-score f@ f<
+  if
+    1
+  else
+    0
+  then ;
+
 : gaussian-pdf ( i k -- r )
   locals| k i |
   i sample@ k mean@ f-
@@ -68,11 +136,12 @@ fvariable current-ll
   SQRT-2PI k sigma@ f* f/ ;
 
 : sample-likelihood ( i -- r )
+  locals| sample-index |
   0e0
   n-clusters 0 do
-    over i gaussian-pdf f+
+    sample-index i gaussian-pdf f+
   loop
-  nip ;
+;
 
 : log-likelihood ( -- r )
   0e0
@@ -192,6 +261,13 @@ fvariable current-ll
     cr
   loop ;
 
+: .histogram-demo ( -- )
+  cr ." Histogram divergence demo (page 17 / eq. 1):" cr
+  init-histograms
+  ."   D(query, view-a) = " hist-query hist-view-a hist-divergence f. cr
+  ."   D(query, view-b) = " hist-query hist-view-b hist-divergence f. cr
+  ."   nearest view index = " nearest-view . cr ;
+
 : run-em ( -- iterations )
   init-params
   max-iterations 0 do
@@ -205,6 +281,8 @@ fvariable current-ll
 
 : main ( -- )
   cr ." Running EM clustering demo in gforth" cr
+  .histogram-demo
+  cr ." EM clustering demo:" cr
   run-em
   ." Iterations: " . cr
   ." Log-likelihood: " current-ll f@ f. cr
